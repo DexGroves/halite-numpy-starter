972946fc47f547401c4e1cfb986df08d9a8d4b23
Dex Groves <Dex.Groves@gmail.com>
2016-12-21 18:56:17 +0000

    Fix everything

--- hlt.py
+++ hlt.py
@@ -1,15 +1,16 @@
 """
 A Python-based Halite starter-bot framework.
-
 A barebones, numpy-inspired fork of erdman's hlt.py that focuses on
 assembling prod, strn and owner numpy matrices as quickly as possible.
-
 Moves are tuples of x, y and dir, where dir behaves as in the
 base starter.
+
+GameMap.prod and GameMap.strn are the production and strength respectively.
+GameMap.owner contains the each cell's owner.
 """
 
 import sys
-from numpy import array, zeros, zeros_like
+import numpy as np
 from collections import namedtuple
 
 
@@ -17,33 +18,37 @@ Move = namedtuple('Move', 'x y dir')
 
 
 class GameMap:
-    """Hold prod, strn and owners and update on each new frame."""
-    def __init__(self, size_string, prod_string, map_string=None):
+    """Hold prod, strn and owners as separate x*y numpy matrices
+    and update on each new frame.
+    """
+
+    def __init__(self, size_string, prod_string, my_id):
         self.width, self.height = tuple(map(int, size_string.split()))
+        self.my_id = my_id
 
         prod = [int(p) for p in prod_string.split()]
-        self.prod = array(prod, dtype=int).reshape((self.width, self.height))
+        self.prod = np.array(prod, dtype=int).reshape((self.height, self.width)).T
 
-        self.get_frame(map_string)
+        self.get_frame()
 
     def get_frame(self, map_string=None):
         if map_string is None:
             map_string = get_string()
 
         split_string = map_string.split()
-        owners = zeros(self.width * self.height, dtype=int)
+        owners = np.zeros(self.width * self.height, dtype=int)
 
         ctr, strloc = 0, 0
         while ctr < self.width * self.height:
             increment = int(split_string[strloc])
             owner_id = int(split_string[strloc + 1])
-            owners[ctr:increment] = owner_id
+            owners[ctr:(ctr + increment)] = owner_id
             ctr += increment
             strloc += 2
-        self.owners = owners.reshape((self.width, self.height))
+        self.owners = owners.reshape((self.height, self.width)).T
 
         strn = [int(s) for s in split_string[strloc:]]
-        self.strn = array(strn, dtype=int).reshape((self.width, self.height))
+        self.strn = np.array(strn, dtype=int).reshape((self.height, self.width)).T
 
 
 def send_string(s):
@@ -57,9 +62,9 @@ def get_string():
 
 
 def get_init():
-    playerID = int(get_string())
-    m = GameMap(get_string(), get_string())
-    return playerID, m
+    my_id = int(get_string())
+    m = GameMap(get_string(), get_string(), my_id)
+    return my_id, m
 
 
 def send_init(name):
